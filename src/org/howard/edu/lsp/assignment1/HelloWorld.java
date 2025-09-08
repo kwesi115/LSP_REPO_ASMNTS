63de23d24b89bf72fd145082c6e9c0659794eb4f
kwesi115 <kwesimichai1@gmail.com>
2025-09-07 22:30:54 -0400

    changing structure

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -7,13 +7,5 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }}
-
-    
-
-    
-
-    
-        
-            
-        
+    }
+}
